a79d1b675dde8e89cf4aef4fc55755694ca5bb5a
Abu Muhsin <shittuabdulmujeeb@gmail.com>
2020-08-29 10:21:44 +0100

    initial commit

--- lib/sec.b.dart
+++ lib/sec.b.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-class Student extends StatelessWidget {
+class deleteStudent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
